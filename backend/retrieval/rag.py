5c9b2424ded78995c1539506a13b09a2147f0d22
sangwonYoon <yoonsw0532@naver.com>
2024-04-02 10:34:31 +0900

    feat: implement multi query

--- backend/retrieval/rag.py
+++ backend/retrieval/rag.py
@@ -1,39 +1,56 @@
+import logging
 import os
 import sys
+
+from langchain.chat_models import ChatOpenAI
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.vectorstores import Pinecone
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from utils.setup import setup_db, setup_embedding, setup_pinecone
 
 
-def retrieve_doc(question, db, vector_db, embedding, top_k):
-    embedded_question = embedding.embed_documents([question])
+def retrieve_doc(question, db, pc_index, embedding, llm, top_k):
+    # embedded_question = embedding.embed_documents([question])
 
-    query_result = vector_db.query(
-        vector=embedded_question,
-        top_k=top_k,
-        include_values=False,
-        include_metadata=True
+    # query_result = pc_index.query(
+    #     vector=embedded_question,
+    #     top_k=top_k,
+    #     include_values=False,
+    #     include_metadata=True
+    # )
+
+    vector_db = Pinecone(pc_index, embedding.embed_query, "content")
+
+    retriever_from_llm = MultiQueryRetriever.from_llm(
+        retriever=vector_db.as_retriever(), llm=llm
     )
 
-    cursor = db.cursor()
-    retrieved_docs = []
+    retrieved_docs = retriever_from_llm.get_relevant_documents(query=question)
 
-    for result in query_result.matches:
-        document_index = int(result.metadata["document-index"])
-        title = result.metadata["title"]
-        score = result.score
-        sql = f"select content from documents where document_index={document_index}"
-        cursor.execute(sql)
-        content = cursor.fetchone()
+    # cursor = db.cursor()
+    # retrieved_docs = []
 
-        temp_result = {"score": score, "title": title, "content": content[0]}
-        retrieved_docs.append(temp_result)
+    # for result in query_result.matches:
+    #     document_index = int(result.metadata["document-index"])
+    #     title = result.metadata["title"]
+    #     score = result.score
+    #     sql = f"select content from documents where document_index={document_index}"
+    #     cursor.execute(sql)
+    #     content = cursor.fetchone()
+
+    #     temp_result = {"score": score, "title": title, "content": content[0]}
+    #     retrieved_docs.append(temp_result)
 
     return retrieved_docs
 
 
 if __name__ == "__main__":
+
+    logging.basicConfig()
+    logging.getLogger("langchain.retrievers.multi_query").setLevel(logging.INFO)
+
     # mysql
     db = setup_db()
 
@@ -42,12 +59,14 @@ if __name__ == "__main__":
 
     # openai
     embedding = setup_embedding()
+    llm = ChatOpenAI()
 
     question = input("질문을 입력하세요: ")
 
-    result = retrieve_doc(question, db, pc_index, embedding, top_k=3)
+    result = retrieve_doc(question, db, pc_index, embedding, llm=llm, top_k=3)
 
-    for r in result:
-        print(f"score: {r['score']}\n\ntitle: {r['title']}\n\ncontent: {r['content']}\n\n")
+    # for r in result:
+    #     print(f"score: {r['score']}\n\ntitle: {r['title']}\n\ncontent: {r['content']}\n\n")
+    print(result)
 
     db.close()
--- backend/retrieval/rag_prototype.py
+++ backend/retrieval/rag_prototype.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import streamlit as st
+from langchain.chat_models import ChatOpenAI
 from rag import retrieve_doc
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
@@ -16,12 +17,13 @@ pc_index = setup_pinecone()
 
 # openai
 embedding = setup_embedding()
+llm = ChatOpenAI()
 
 st.title('RAG Prototype')
 
 question = st.text_input('질문을 입력하세요:')
 
 if question:
-    st.write(retrieve_doc(question, db, pc_index, embedding, top_k=5))
+    st.write(retrieve_doc(question, db, pc_index, embedding, llm=llm, top_k=5))
 
 db.close()
