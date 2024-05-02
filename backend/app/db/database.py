8b26ddbbbb2d44a77d3e972bb8405d908adb1cb6
Hyeonseung-lee <dlgustmd3590@gmail.com>
2024-05-02 20:33:51 +0900

    feat:models,main,database and connect DBMS

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,23 +1,24 @@
-# 필요한 라이브러리 import하기
+# sqlalchemy database connection
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# load local .env
 from dotenv import load_dotenv
 import os
 
+# get the base directory
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# load the .env file
 load_dotenv(os.path.join(BASE_DIR, ".env"))
+# get the database url
+DB_URL = os.getenv("DB_URL")
 
-# SQLAlchemy 사용할 DB URL 생성하기
-SQLALCHEMY_DATABASE_URL = os.environ["DB_URL"]
+# create the engine
+engine = create_engine(DB_URL)
 
-# SQLAlchemy engine 생성하기
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
-
-# DB 세션 생성하기
+# create the session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base class 생성하기
+# create the base
 Base = declarative_base()
