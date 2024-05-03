bd26e3657206900a6e64c2a20c5f1960021f4e1c
changyeop1 <changyeop@comcom.ai>
2024-05-03 16:16:07 +0900

    feat: apply user router

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 
-from api.routes import templates
+from api.routes import templates, users
 
 api_router = APIRouter()
 api_router.include_router(templates.router, prefix="/templates")
+api_router.include_router(users.router, prefix="/user")
