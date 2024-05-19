5dc3dbca75ea87909c6b500eaf11488ff60052b9
sangwonYoon <yoonsw0532@naver.com>
2024-05-19 21:17:24 +0900

    feat: Implement update.sh

---
+++ update.sh
@@ -0,0 +1,7 @@
+cd ./capstone-2024-05
+git pull
+cd ./backend
+pip install -r requirements.txt
+cd ./app
+pkill -9 gunicorn
+gunicorn -k uvicorn.workers.UvicornWorker --access-logfile ./gunicorn-access.log main:app --bind 0.0.0.0:8001 --workers 2 --daemon --timeout 300
